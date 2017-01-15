8ae84204a4936c013064c5cb1c714bbae29e1eb5
HarrisSchwartz <hfschwartz21@gmail.com>
2017-01-15 01:14:11 -0500

    added "sum"
    more random gifs

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -4,7 +4,6 @@ class main {
      constructor() {
           main.prepApp();
           main.handleRoll();
-          main.handleGifs();
           main.handleRollResults();
           main.handleRedo();
           main.handleNewRoll();
@@ -28,28 +27,25 @@ class main {
 
      static handleRoll() {
           document.getElementById('roll').addEventListener('click', () => {
+               var random = Math.floor(Math.random() * 5);
+               if (random == 1) {
+                    document.getElementById('rollgif').style.display = 'block';
+               } else if (random == 2) {
+                    document.getElementById('rollgif2').style.display = 'block';
+               } else if (random == 3) {
+                    document.getElementById('rollgif3').style.display = 'block';
+               } else if (random == 4) {
+                    document.getElementById('rollgif4').style.display = 'block';
+               } else {
+                    document.getElementById('rollgif5').style.display = 'block';
+               }
+          });
+          document.getElementById('roll').addEventListener('click', () => {
                document.getElementById('bottom').style.display = 'block';
                document.getElementById('top').style.display = 'none';
                document.getElementById('results').style.display = 'block';
                document.getElementById('redo').style.display = 'block';
                document.getElementById('newRoll').style.display = 'block';
-          });
-     }
-
-     static handleGifs() {
-          document.getElementById('roll' || 'redo').addEventListener('click', () => {
-          var random = Math.floor(Math.random() * 5);
-          if (random == 1) {
-               document.getElementById('rollgif').style.display = 'block';
-          } else if (random == 2) {
-               document.getElementById('rollgif2').style.display = 'block';
-          } else if (random == 3) {
-               document.getElementById('rollgif3').style.display = 'block';
-          } else if (random == 4) {
-               document.getElementById('rollgif4').style.display = 'block';
-          } else {
-               document.getElementById('rollgif5').style.display = 'block';
-          }
           });
      }
 
@@ -60,72 +56,112 @@ class main {
 
                if (document.getElementById('d4').checked) {
                while (indivResults.length < numDice) {
-                    indivResults[indivResults.length] = Math.floor(Math.random() * 5);
+                    indivResults[indivResults.length] = Math.floor(1 + Math.random() * 4);
                }
           } else if (document.getElementById('d6').checked) {
                while (indivResults.length < numDice) {
-                    indivResults[indivResults.length] = Math.floor(Math.random() * 7);
+                    indivResults[indivResults.length] = Math.floor(1 + Math.random() * 6);
                }
           } else if (document.getElementById('d8').checked) {
                while (indivResults.length < numDice) {
-                    indivResults[indivResults.length] = Math.floor(Math.random() * 9);
+                    indivResults[indivResults.length] = Math.floor(1 + Math.random() * 8);
                }
           } else if (document.getElementById('d10').checked) {
                while (indivResults.length < numDice) {
-                    indivResults[indivResults.length] = Math.floor(Math.random() * 11);
+                    indivResults[indivResults.length] = Math.floor(1 + Math.random() * 10);
                }
           } else if (document.getElementById('d12').checked) {
                while (indivResults.length < numDice) {
-                    indivResults[indivResults.length] = Math.floor(Math.random() * 13);
+                    indivResults[indivResults.length] = Math.floor(1 + Math.random() * 12);
                }
           } else if (document.getElementById('d20').checked) {
                while (indivResults.length < numDice) {
-                    indivResults[indivResults.length] = Math.floor(Math.random() * 21);
+                    indivResults[indivResults.length] = Math.floor(1 + Math.random() * 20);
                }
           } else if (document.getElementById('d100').checked) {
                while (indivResults.length < numDice) {
-                    indivResults[indivResults.length] = Math.floor(Math.random() * 101);
+                    indivResults[indivResults.length] = Math.floor(1 + Math.random() * 100);
                }
           }
-          document.getElementById('indivResults').innerText = (indivResults.toString(indivResults));
+          document.getElementById('indivResults').innerText = (indivResults.toString(indivResults).replace(/,/g, ', '));
+               document.getElementById('sumResults').innerText = (indivResults.reduce(function (a,b) {
+                    return a + b;
+               }, 0));
      });
 }
 
      static handleRedo() {
+          document.getElementById('redo').addEventListener('click', () => {
+               var random = Math.floor(Math.random() * 5);
+               if (random == 1) {
+                    document.getElementById('rollgif').style.display = 'block';
+                    document.getElementById('rollgif2').style.display = 'none';
+                    document.getElementById('rollgif3').style.display = 'none';
+                    document.getElementById('rollgif4').style.display = 'none';
+                    document.getElementById('rollgif5').style.display = 'none';
+               } else if (random == 2) {
+                    document.getElementById('rollgif2').style.display = 'block';
+                    document.getElementById('rollgif').style.display = 'none';
+                    document.getElementById('rollgif3').style.display = 'none';
+                    document.getElementById('rollgif4').style.display = 'none';
+                    document.getElementById('rollgif5').style.display = 'none';
+               } else if (random == 3) {
+                    document.getElementById('rollgif3').style.display = 'block';
+                    document.getElementById('rollgif').style.display = 'none';
+                    document.getElementById('rollgif2').style.display = 'none';
+                    document.getElementById('rollgif4').style.display = 'none';
+                    document.getElementById('rollgif5').style.display = 'none';
+               } else if (random == 4) {
+                    document.getElementById('rollgif4').style.display = 'block';
+                    document.getElementById('rollgif').style.display = 'none';
+                    document.getElementById('rollgif2').style.display = 'none';
+                    document.getElementById('rollgif3').style.display = 'none';
+                    document.getElementById('rollgif5').style.display = 'none';
+               } else {
+                    document.getElementById('rollgif5').style.display = 'block';
+                    document.getElementById('rollgif').style.display = 'none';
+                    document.getElementById('rollgif2').style.display = 'none';
+                    document.getElementById('rollgif3').style.display = 'none';
+                    document.getElementById('rollgif4').style.display = 'none';
+               }
+          });
           document.getElementById('redo').addEventListener('click', () => {
                var indivResults = [];
                var numDice = document.getElementById('slider').value;
 
                if (document.getElementById('d4').checked) {
                     while (indivResults.length < numDice) {
-                         indivResults[indivResults.length] = Math.floor(Math.random() * 5);
+                         indivResults[indivResults.length] = Math.floor(1 + Math.random() * 4);
                     }
                } else if (document.getElementById('d6').checked) {
                     while (indivResults.length < numDice) {
-                         indivResults[indivResults.length] = Math.floor(Math.random() * 7);
+                         indivResults[indivResults.length] = Math.floor(1 + Math.random() * 6);
                     }
                } else if (document.getElementById('d8').checked) {
                     while (indivResults.length < numDice) {
-                         indivResults[indivResults.length] = Math.floor(Math.random() * 9);
+                         indivResults[indivResults.length] = Math.floor(1 + Math.random() * 8);
                     }
                } else if (document.getElementById('d10').checked) {
                     while (indivResults.length < numDice) {
-                         indivResults[indivResults.length] = Math.floor(Math.random() * 11);
+                         indivResults[indivResults.length] = Math.floor(1 + Math.random() * 10);
                     }
                } else if (document.getElementById('d12').checked) {
                     while (indivResults.length < numDice) {
-                         indivResults[indivResults.length] = Math.floor(Math.random() * 13);
+                         indivResults[indivResults.length] = Math.floor(1 + Math.random() * 12);
                     }
                } else if (document.getElementById('d20').checked) {
                     while (indivResults.length < numDice) {
-                         indivResults[indivResults.length] = Math.floor(Math.random() * 21);
+                         indivResults[indivResults.length] = Math.floor(1 + Math.random() * 20);
                     }
                } else if (document.getElementById('d100').checked) {
                     while (indivResults.length < numDice) {
-                         indivResults[indivResults.length] = Math.floor(Math.random() * 101);
+                         indivResults[indivResults.length] = Math.floor(1 + Math.random() * 100);
                     }
                }
-               document.getElementById('indivResults').innerText = (indivResults.toString(indivResults));
+               document.getElementById('indivResults').innerText = (indivResults.toString(indivResults).replace(/,/g, ', '));
+               document.getElementById('sumResults').innerText = (indivResults.reduce(function (a,b) {
+                    return a + b;
+               }, 0));
           });
      }
 
